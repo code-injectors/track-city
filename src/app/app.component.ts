c58cb31b55d52d04f76f77fb9a7af914f71451fe
Nectarios <nekkon01@gmail.com>
2017-05-06 21:09:30 +0300

    updated loading

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-
+import { SDK } from './app.sdk';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+    constructor(public sdk:SDK){}
 }
