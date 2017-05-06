e98560005fc21985984b6d8c41e01c22ab6804f1
Nectarios <nekkon01@gmail.com>
2017-05-06 16:46:44 +0300

    created first call of sdk

--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
+import { SDK } from './../../app.sdk';
 
 @Component({
     selector: 'app-login',
@@ -8,14 +9,16 @@ import { Router, ActivatedRoute } from '@angular/router';
 })
 export class loginView implements OnInit {
 
-    constructor(public router:Router) { }
+    constructor(public router:Router,public sdk:SDK) { }
 
     ngOnInit() {
     }
 
     login(){
-
-        this.router.navigate(['/admin']);
+        this.sdk.getUsers().subscribe(result => {
+            console.log(result);
+        });
+        //this.router.navigate(['/admin']);
     }
 
 }
