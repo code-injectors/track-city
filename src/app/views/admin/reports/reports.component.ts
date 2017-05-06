7f7805ca990a020423a97cc70e89cc72f5a034cc
Nectarios <nekkon01@gmail.com>
2017-05-06 14:33:06 +0300

    added filters

--- src/app/views/admin/reports/reports.component.ts
+++ src/app/views/admin/reports/reports.component.ts
@@ -7,77 +7,92 @@ import { Component, OnInit } from '@angular/core';
 })
 export class reportsView implements OnInit {
 
-  lat:Number = 41.08247;
-  lng:Number = 23.5437952;
-  zoom:Number = 16;
+    lat:Number = 41.08247;
+    lng:Number = 23.5437952;
+    zoom:Number = 16;
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
-  clickedMarker(label: string, index: number) {
-    console.log(`clicked the marker: ${label || index}`)
-  }
-  
-  mapClicked($event: any) {
-    this.markers.push({
-      lat: $event.coords.lat,
-      lng: $event.coords.lng
-    });
-  }
-  
-  markerDragEnd(m: marker, $event: MouseEvent) {
-    console.log('dragEnd', m, $event);
-  }
-  
-  markers: marker[] = [
-	  {
-		  lat: 51.673858,
-		  lng: 7.815982,
-		  label: 'A',
-		  draggable: true
-	  },
-	  {
-		  lat: 51.373858,
-		  lng: 7.215982,
-		  label: 'B',
-		  draggable: false
-	  },
-	  {
-		  lat: 51.723858,
-		  lng: 7.895982,
-		  label: 'C',
-		  draggable: true
-	  }
-  ]
+    clickedMarker(label: string, index: number) {
+      console.log(`clicked the marker: ${label || index}`)
+    }
+    
+    mapClicked($event: any) {
+      this.markers.push({
+        lat: $event.coords.lat,
+        lng: $event.coords.lng
+      });
+    }
+    
+    markerDragEnd(m: marker, $event: MouseEvent) {
+      console.log('dragEnd', m, $event);
+    }
+    
+    markers: marker[] = [
+      {
+        lat: 51.673858,
+        lng: 7.815982,
+        label: 'A',
+        draggable: true
+      },
+      {
+        lat: 51.373858,
+        lng: 7.215982,
+        label: 'B',
+        draggable: false
+      },
+      {
+        lat: 51.723858,
+        lng: 7.895982,
+        label: 'C',
+        draggable: true
+      }
+    ]
 
-  reports: any[] = [
-    {
-        title: 'test1',
-        description: 'description1 sal;kjflk;aglkjhklhj',
-        longitude: 8577865,
-        latitude: 85758765,
-        category: 'dummy',
-        creator: 'tzinos',
-    },
-    {
-        title: 'test2',
-        description: 'description1 sal;kjflk;aglkjhklhj',
-        longitude: 8577865,
-        latitude: 85758765,
-        category: 'dummy',
-        creator: 'tzinos',
-    },
-    {
-        title: 'test3',
-        description: 'description1 sal;kjflk;aglkjhklhj',
-        longitude: 8577865,
-        latitude: 85758765,
-        category: 'dummy',
-        creator: 'tzinos',
-    },
-  ]
+    reports: any[] = [
+      {
+          title: 'test1',
+          description: 'description1 sal;kjflk;aglkjhklhj',
+          longitude: 8577865,
+          latitude: 85758765,
+          category: 'dummy',
+          creator: 'tzinos',
+          thumb_up: 120,
+          thumb_down: 22,
+      },
+      {
+          title: 'test2',
+          description: 'description1 sal;kjflk;aglkjhklhj',
+          longitude: 8577865,
+          latitude: 85758765,
+          category: 'dummy',
+          creator: 'tzinos',
+          thumb_up: 79,
+          thumb_down: 8,
+      },
+      {
+          title: 'test3',
+          description: 'description1 sal;kjflk;aglkjhklhj',
+          longitude: 8577865,
+          latitude: 85758765,
+          category: 'dummy',
+          creator: 'tzinos',
+          thumb_up: 8,
+          thumb_down: 4,
+      },
+    ]
+
+    filters: any[] = [
+        {
+            value: 'All'
+        },
+        {
+            value: 'Specific filter'
+        }
+    ]
 }
 
 interface marker {
